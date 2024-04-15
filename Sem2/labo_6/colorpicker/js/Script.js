c858693b4cc15f444ea7bcc58828e876a5eb835e
lambertR <rune.lambert@student.vives.be>
2024-04-15 08:53:39 +0200

    Update Script.js

--- Sem2/labo_6/colorpicker/js/Script.js
+++ Sem2/labo_6/colorpicker/js/Script.js
@@ -1,16 +1,15 @@
 const setup = () =>{
     let slider = document.getElementsByClassName("slider");
     let button = document.getElementById("button");
-    let removeButton = document.getElementsByTagName("button");
-
+    
     button.addEventListener("click", addcolor);
-
-    slider[0].addEventListener("input", functie);
-    slider[0].addEventListener("change",functie)
-    slider[1].addEventListener("input", functie);
-    slider[1].addEventListener("change",functie)
-    slider[2].addEventListener("input", functie);
-    slider[2].addEventListener("change",functie)
+    
+    
+    for(let i=0; i<slider.length;i++){
+        slider[i].addEventListener("input", functie);
+        slider[i].addEventListener("change",functie)
+    }
+    
     functie();
 
 
@@ -30,33 +29,56 @@ const functie = () =>{
     text[2].innerHTML = "255 Blue: "+slider2;
 
     box[0].style.backgroundColor = "rgb("+slider0+","+slider1+","+slider2+")";
-    
+    box[0].addEventListener("click",original)
 }
+const original = (click)=>{
+    
+    let slider = document.getElementsByClassName("slider");
+    let box = document.getElementsByClassName("box");
+    let slider0 = slider[0].value;
+    let slider1 = slider[1].value;
+    let slider2 = slider[2].value;
+    
+    box[0].style.backgroundColor = "rgb("+slider0+","+slider1+","+slider2+")";
 
+}
 const addcolor = () =>{
 
     let item = document.getElementsByClassName ("items")[0];
     let slider = document.getElementsByClassName("slider");
     const colorBox = document.createElement("div");
     const button = document.createElement("button");
+    
     let slider0 = slider[0].value;
     let slider1 = slider[1].value;
     let slider2 = slider[2].value;
 
     colorBox.style.backgroundColor = "rgb("+slider0+","+slider1+","+slider2+")";
+    colorBox.className = "colorBox";
     button.style.float="right";
-    button.innerHTML="X";
-    button.name="remove";
-   
+    button.innerHTML="x";
+    button.type="button";
+    button.id="removeButton";
+    button.className="remove";
     
     item.appendChild(colorBox);
     colorBox.appendChild(button)
 
-
+    button.addEventListener("click",remove);
+    item.addEventListener("click",showColor);
 
 }
 
-const remove=()=>{
-    let button = document.get
+const remove = (click) =>{
+    
+   
+    click.target.parentNode.remove();
+
+}
+const showColor = (click)=>{
+    let box = document.getElementsByClassName("box");
+    let colors = click.target.style.backgroundColor;
+    console.log(colors);
+    box[0].style.backgroundColor = click.target.style.backgroundColor;
 }
 window.addEventListener("load",setup);
